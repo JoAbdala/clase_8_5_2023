c0bdc0c3d53349c9f512bb984e02aeb0c2fc3e40
¨JoAbdala¨ <marsofio@hotmail.com>
2023-05-11 13:32:26 -0300

    primera vista de python

---
+++ index.py
@@ -0,0 +1,21 @@
+num = 1
+num2 =2.25
+numC= "1"
+palabra = "la casa bonita"
+palabra2 = "pero la casa es vieja"
+logico =True 
+letra= 'c'
+
+
+print(palabra)
+
+print("mostramos el numero:", num , "peso")
+print("mostramos el numero:"+numC+ " peso")
+print(f"mostramos el numero:{num} peso, y el otro valor :{num2} pesos")
+print("mostramos el numero:"+numC+" peso")
+print(palabra + " "+ palabra2)
+print(palabra, end="\n")
+res= num + num2
+
+print(palabra2)
+print(res)
